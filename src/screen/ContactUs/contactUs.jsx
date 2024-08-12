9b95f8cd2947dee1f1bdaea58dcfe59eaab5f06b
Molaei <molaie.web.developer@gmail.com>
2024-08-13 00:51:51 +0100

    Create contact us page

--- src/screen/ContactUs/contactUs.jsx
+++ src/screen/ContactUs/contactUs.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import TitleSection from "../../components/common/partials/TitleSection/titleSection"
+import { Main, Map, Sidebar } from "../../components/pages/ContactUs"
 
 const ContactUs = () => {
   return (
-    <div>
-      Contact Us
-    </div>
+    <>
+      <TitleSection title="تماس با ما" />
+      <div className="sm:px-44 px-16 w-full flex flex-wrap gap-y-16 my-20">
+        <div className="w-full flex justify-between gap-x-6">
+          <Sidebar />
+          <Main />
+        </div>
+        {/* Map */}
+        <div className="w-full h-[400px] overflow-hidden flex justify-center rounded-xl">
+          <Map />
+        </div>
+      </div>
+    </>
   )
 }
 
