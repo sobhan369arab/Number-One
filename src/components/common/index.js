c856928d9a8c4503e068aa884fe5add35562e1d8
Molaei <molaie.web.developer@gmail.com>
2024-08-22 08:36:48 +0100

    export new components

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,19 @@
 import BlueButton from "./BlueButton";
 import LogoGroup from "./LogoGroup";
 import { PaginatedItems } from "./Pageinate";
-export { BlueButton, LogoGroup, PaginatedItems };
+import CreateAccordionItem from "./createAccordionItem";
+import ImportantWord from "./importantWord";
+import InformedPoster from "./informedPoster";
+import OptionParts from "./optionParts";
+import TeacherCard from "./teacherCard";
+
+export {
+  BlueButton,
+  LogoGroup,
+  PaginatedItems,
+  CreateAccordionItem,
+  ImportantWord,
+  InformedPoster,
+  OptionParts,
+  TeacherCard,
+};
