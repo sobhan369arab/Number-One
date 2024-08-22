f4711c8c8e3b9d5ec0f37884a222dedd354d2b02
Molaei <molaie.web.developer@gmail.com>
2024-08-22 07:31:25 +0100

    Finished with out blog item and responsive and english language

---
+++ src/components/pages/landing/lastBlogs.jsx
@@ -0,0 +1,12 @@
+const LastBlogs = () => {
+  return (
+    <div className="w-full py-28 lg:px-44 sm:px-16 px-8 flex flex-wrap gap-y-4 justify-center bg-gradientBackground bg-cover bg-center bg-no-repeat">
+      <span className="tag">اخبار و وبلاگ ها</span>
+      <h1 className="title-text text-center">آخرین خبر ما</h1>
+      <p className="description-text">هنگامی که چاپگر شناخته شده یک گالری از نوع scrambl edmake گرفت</p>
+      <div className="w-full h-80 bg-yellow-500"></div>
+    </div>
+  )
+}
+
+export default LastBlogs
