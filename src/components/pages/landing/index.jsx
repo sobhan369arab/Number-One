5adf0f892126919a607fcf2acb548ace040ab306
Molaei <molaie.web.developer@gmail.com>
2024-08-22 08:41:13 +0100

    finished export all component about landing page

---
+++ src/components/pages/landing/index.jsx
@@ -0,0 +1,21 @@
+import HeroSection from "./heroSection";
+import Categories from "./categories";
+import AboutUs from "./aboutUs";
+import TopCourses from "./topCourses.jsx";
+import SkilledTeachers from "./skilledTeachers";
+import Statistics from "./statistics.jsx";
+import Faq from "./faq.jsx";
+import HowToStart from "./howToStart.jsx";
+import LastBlogs from "./lastBlogs.jsx";
+
+export {
+    HeroSection,
+    Categories,
+    AboutUs,
+    TopCourses,
+    SkilledTeachers,
+    Statistics,
+    Faq,
+    HowToStart,
+    LastBlogs
+}
