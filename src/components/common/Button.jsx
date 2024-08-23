e344651a3711e0906bfc3575a54fbeaf18c83dad
Molaei <molaie.web.developer@gmail.com>
2024-08-23 16:48:01 +0100

    add shadow to variants

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,14 +2,14 @@ import { useTranslation } from "react-i18next";
 import { ArrowLeftIcon } from "../../core/icon";
 
 export const variantButton = {
-    "purple": "bg-purpleCustom text-white",
-    "yellow": "bg-yellowCustom text-black",
+    "purple": "bg-purpleCustom text-white buttonPurple_shadow",
+    "yellow": "bg-yellowCustom text-black buttonYellow_shadow",
 }
 
 const Button = ({ text, arrowColor, variant, style }) => {
     const { t, i18n } = useTranslation();
     return (
-        <button className={`rounded-3xl pb-3 pt-1.5 px-5 flex items-center gap-x-2 BoxShadow-BtnBlue duration-200 ${variantButton?.[variant]} ${style}`}>
+        <button className={`rounded-3xl pb-3 pt-1.5 px-5 flex items-center gap-x-2 duration-200 ${variantButton?.[variant]} ${style}`}>
             <span className="text-sm">{t(text)}</span>
             <ArrowLeftIcon stroke={arrowColor} height="12px" className={i18n.language === "en" ? "reverse-img" : ""} />
         </button>
