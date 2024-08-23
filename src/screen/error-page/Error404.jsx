03c3ac6403445766be94c80ba6d9cc0019aeee02
Molaei <molaie.web.developer@gmail.com>
2024-08-23 17:29:11 +0100

    change button and title-section url

--- src/screen/error-page/Error404.jsx
+++ src/screen/error-page/Error404.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next"
-import { BlueButton } from "../../components/common";
-import TitleSection from "../../components/partials/TitleSection/titleSection";
+import { Button } from "../../components/common";
+import TitleSection from "../../components/partials/title-section/TitleSection";
 import { Error404Icon } from "../../core/icon";
 
 const Error404 = () => {
@@ -8,11 +8,11 @@ const Error404 = () => {
     return (
         <>
         <TitleSection title={'error'}/>
-        <div className="m-auto sm:my-16 my-12 w-fit font-semibold cursor-default">
-            <Error404Icon className="sm:w-[420px] w-80 mx-auto " />
-            <h1 className="mb-1 sm:text-3xl text-xl text-center">{t('error')}!</h1>
-            <h1 className="sm:text-3xl text-xl text-center">{t('ErrorDesc')}</h1>
-            <BlueButton ArrowColor={"white"} text={'GoHomePage'} Class={'w-48 bg-purpleCustom text-white mt-10 m-auto'}/>
+        <div className="m-auto sm:my-16 my-12 w-fit flex flex-wrap  text-titleColor gap-y-4 justify-center font-semibold cursor-default">
+            <Error404Icon className="sm:w-[420px] w-80" />
+            <h1 className="w-full sm:text-3xl text-xl text-center">{t('error')}!</h1>
+            <h1 className="w-full sm:text-3xl text-xl text-center">{t('ErrorDesc')}</h1>
+            <Button arrowColor={"#fff"} variant="purple" text={'GoHomePage'}/>
         </div>
         </>
     )
