099bfb13e11a45537cba2b0082065c99978ef964
Molaei <molaie.web.developer@gmail.com>
2024-08-23 16:48:19 +0100

    add Custom map for pages

---
+++ src/components/common/CustomMap.jsx
@@ -0,0 +1,11 @@
+const CustomMap = ({ width, height }) => {
+    return (
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d400.39789939310225!2d53.06447394522351!3d36.59789666991099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8515a7cb36f467%3A0x8a63125684473844!2z2b7amNmI2YfYtNqv2KfZhyDYs9m-2YfYsQ!5e0!3m2!1sen!2s!4v1723475793806!5m2!1sen!2s"
+            width={width}
+            height={height}>
+        </iframe>
+    )
+}
+
+export default CustomMap
