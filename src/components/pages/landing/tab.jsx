59d3a1aa2ed10699d2c09dd0bb71469e15359e6c
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:45:26 +0100

    use Query Parameter in tab work

--- src/components/pages/landing/tab.jsx
+++ src/components/pages/landing/tab.jsx
@@ -1,12 +1,17 @@
-const Tab = ({ name, title }) => {
+import { useSearchParams } from "react-router-dom"
+
+const Tab = ({ name, title, value }) => {
+    const [searchParams, setSearchParams] = useSearchParams({ category: "همه دوره ها" })
+    const categoryNow = searchParams.get("category")
     // get the title and filter item
-    function callBack(title) {
-        console.log(title)
+    function callBack(value) {
+        console.log(value)
+        setSearchParams({category: value})
     }
 
     return (
-        <label className="mx-5 pb-3 h-full relative text-neutral-400 cursor-pointer">
-            <input onChange={() => { callBack(title) }} type="radio" name={"tab-" + name} className=" hidden peer/tab" />
+        <label className="mobile:mx-0 mobile:text-sm mx-5 pb-3 h-full relative text-neutral-400 cursor-pointer">
+            <input checked={categoryNow === value ? true : false} onChange={() => { callBack(value) }} type="radio" name={"tab-" + name} className=" hidden peer/tab" />
             <span className="peer-checked/tab:text-black">{title}</span>
             <span className="w-full h-1.5 transition-all duration-300 peer-checked/tab:bg-purpleCustom rounded-full absolute -bottom-0.5 right-0 z-10"></span>
         </label>
