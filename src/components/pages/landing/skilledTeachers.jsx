508d6cc517b118675155892fdb74b53f443b598f
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:44:25 +0100

    Config with i18n and use Label component and add slider in small device

--- src/components/pages/landing/skilledTeachers.jsx
+++ src/components/pages/landing/skilledTeachers.jsx
@@ -1,23 +1,40 @@
-import { BlueButton, TeacherCard } from "../../common"
+import { Button, Label, TeacherCard, SwiperSlider } from "../../common"
+import { SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import MediaQuery from "react-responsive";
+import { useTranslation } from "react-i18next";
 
 const SkilledTeachers = () => {
+    const { t } = useTranslation()
     return (
-        <div className="w-full flex gap-x-40 py-28 lg:px-44 sm:px-16 px-8">
-            <div className="min-w-[280px] w-[280px] h-fit flex flex-wrap gap-y-4">
-                <span className="tag">معلم های ماهر</span>
-                <h1 className="w-full title-text">کلاس برتر ما و مربیان خبره در یک مکان</h1>
-                <p className="w-full text-neutral-400">
-                    هنگامی که یک چاپگر ناشناس یک گالری از نوع و
-                    کتاب نمونه درهم درست شده باقی نمانده است
-                    فقط پنج قرن
-                </p>
-                <BlueButton text="همه مربیان را ببینید" Class="mt-5 text-white bg-purpleCustom h-fit" ArrowColor="#fff" />
+        <div className="w-full flex flex-wrap xl:flex-nowrap justify-center xl:justify-between gap-y-12 py-28 lg:px-44 sm:px-16 px-8">
+            <div className="min-w-[280px] w-full xl:w-[280px] h-fit flex justify-center xl:justify-start flex-wrap gap-y-4">
+                <Label text={t("skilledTeachersLabel")} variant="section" />
+                <h1 className="w-full text-center xl:text-start boldStyle_text">{t("skilledTeachersTitle")}</h1>
+                <p className="w-full text-center xl:text-start text-neutral-400">{t("skilledTeachersDesc")}</p>
+                <Button text={t("skilledTeachersBtn")} style="mt-5" variant={"purple"} arrowColor="#fff" />
             </div>
-            <div className="w-3/4 h-fit flex flex-wrap justify-between gap-y-10">
-                <TeacherCard />
-                <TeacherCard />
-                <TeacherCard />
-                <TeacherCard />
+            <div className="w-full xl:w-[640px] h-fit flex flex-wrap justify-between gap-y-10 relative">
+                <MediaQuery minWidth={"700px"}>
+                    <TeacherCard />
+                    <TeacherCard />
+                    <TeacherCard />
+                    <TeacherCard />
+                </MediaQuery>
+                <MediaQuery maxWidth={"700px"}>
+                    <SwiperSlider
+                        perView={1}
+                        arrowColor="#000"
+                        buttonSideLeft="top-[30px] xl:left-[40px] left-[5px] z-20"
+                        buttonSideRight="top-[30px] xl:right-[40px] right-[5px] z-20"
+                        buttonColor="bg-yellowCustom"
+                    >
+                        <SwiperSlide className="justify-center flex"><TeacherCard /></SwiperSlide>
+                        <SwiperSlide className="justify-center flex"><TeacherCard /></SwiperSlide>
+                        <SwiperSlide className="justify-center flex"><TeacherCard /></SwiperSlide>
+                        <SwiperSlide className="justify-center flex"><TeacherCard /></SwiperSlide>
+                    </SwiperSlider>
+                </MediaQuery>
             </div>
         </div>
     )
