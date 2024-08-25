2ac3fe8c536f4d77bb9fa21d9c1fb19021ed569c
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:46:06 +0100

    Config whit i18n and use Label component

--- src/components/pages/landing/topCourses.jsx
+++ src/components/pages/landing/topCourses.jsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next"
+import { Label } from "../../common"
 import Tab from "./tab"
+import { tabOptions } from "../../../core/constants/landing/TopCoursesTab"
 
 const TopCourses = () => {
-    const options = ["همه دوره ها", "طراحی", "تجارت", "توسعه"]
+    const { t, i18n } = useTranslation()
     return (
         <div className="w-full py-28 lg:px-44 sm:px-16 px-8 flex flex-wrap gap-y-4 justify-center bg-gradientBackground bg-cover bg-center bg-no-repeat">
-            <span className="tag">دوره های کلاس برتر</span>
-            <h1 className="title-text text-center">بهترین دوره های آموزشی جهان ما را کاوش کنید</h1>
-            <p className="description-text w-full text-center">امروزه به دلیل آن که ارتباطات فضای مجازی رونق زیادی یافته است</p>
-            <div className=" flex justify-center relative px-20 mt-10">
-                {options.map((obj, index) => <Tab key={index} id={index} name="landing" title={obj} />)}
+            <Label text={t("topCoursesLabel")} variant="section" />
+            <h1 className="boldStyle_text w-full text-center">{t("topCoursesTitle")}</h1>
+            <p className="mediumStyle_text w-full text-center">{t("topCoursesDescription")}</p>
+            <div className="mobile:w-full sm:w-auto flex mobile:justify-evenly justify-center relative mobile:px-0 sm:px-20 mt-10">
+                {tabOptions.map(obj => <Tab key={obj.id} value={obj.value} name="landing" title={i18n.language === "en" ? obj.text[1] : obj.text[0]} />)}
                 <span className="w-full h-1 bg-[#D9D9F3] rounded-full bottom-0 absolute"></span>
             </div>
             <div className="w-full h-80 mt-5 bg-yellow-500"></div>
