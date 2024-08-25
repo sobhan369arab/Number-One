96c5104eefb9e0fbbf036aa4c286efe07d4a5ac4
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:41:35 +0100

    change Typewriter component and add mannequin and change classNames and config with i18n

--- src/components/pages/landing/heroSection.jsx
+++ src/components/pages/landing/heroSection.jsx
@@ -1,39 +1,43 @@
-import { BlueButton, ImportantWord } from "../../common"
+import { useTranslation } from "react-i18next";
+import { Button, ImportantWord } from "../../common"
 import Typewriter from 'typewriter-effect';
+import mannequin_heroSection from "../../../assets/images/mannequin-heroSection.png"
+import { boxItems } from "../../../core/constants/landing/heroSection";
 
 const HeroSection = () => {
-    const typeWriterOptions = {
-        autoStart: true,
-        loop: true,
-        wrapperClassName: "text-2xl",
-        cursorClassName: "text-2xl"
-    }
+    const { t, i18n } = useTranslation()
+    const persianWords = ["یادگیری", "تلاش", "استقامت"]
+    const englishWords = ["Learning", "Effort", "Stamina"]
+
     return (
-        <div className="w-full h-[420px] flex items-center bg-heroSection bg-cover lg:px-44 sm:px-16 px-8">
-            <div className="w-1/2 xl:w-1/3 h-fit flex flex-wrap leading-10 text-2xl">
-                <div className="flex h-fit gap-x-3 items-end">
-                    <h1 className='font-bold'>هرگز از</h1>
-                    <ImportantWord isExplosion={true}>
+        <div className="w-full h-[420px] flex justify-between items-center bg-gradientBackground bg-cover lg:px-44 sm:px-16 px-8">
+            <div className="w-full md:w-[400px] h-fit flex flex-wrap justify-center md:justify-start gap-y-4">
+                <div className="w-full flex justify-center md:justify-start flex-wrap h-fit gap-y-4 md:gap-x-3 items-end">
+                    <h1 className='boldStyle_text w-full text-nowrap text-center md:w-auto'>{t("heroSectionTitle1")}</h1>
+                    <ImportantWord isExplosion={true} explosionStyle="hidden xl:block" holderStyle="h-auto">
                         <Typewriter
-                            onInit={(typewriter) => {
-                                typewriter.typeString('یادگیری')
-                                    .pauseFor(1000)
-                                    .deleteChars(7)
-                                    .typeString('تلاش')
-                                    .pauseFor(1000)
-                                    .deleteChars(4)
-                                    .typeString('استقامت')
-                                    .pauseFor(1000)
-                                    .deleteChars(7)
-                                    .start();
+                            options={{
+                                strings: (i18n.language === "en" ? englishWords : persianWords),
+                                autoStart: true,
+                                loop: true,
                             }}
-                            options={typeWriterOptions}
                         />
                     </ImportantWord>
                 </div>
-                <h1 className='font-bold'>دست نکشید زندگی هرگز از آموزش دست نمی کشد</h1>
-                <p className='w-full text-base text-neutral-500'>هر سفر آموزشی و یادگیری دنبال کردن منحصر به فرد است ما به شما کمک خواهیم کرد</p>
-                <BlueButton text="رایگان آزمایش کنید" Class="mt-10 text-white bg-purpleCustom h-fit" ArrowColor="#fff" />
+                <h1 className='boldStyle_text md:text-start text-center'>{t("heroSectionTitle2")}</h1>
+                <p className='mediumStyle_text md:text-start text-center'>{t("heroSectionDescription")}</p>
+                <Button text={t("heroSectionBtn")} style="mt-5" variant={"purple"} arrowColor="#fff" />
+            </div>
+            <div className="w-[450px] h-full hidden md:flex justify-center items-end relative">
+                <div className={`${i18n.language === "en" ? "left-[-0px]" : "right-[-0px]"} w-44 h-24 flex flex-wrap rounded-lg top-[60px] p-4 heroSection_box_shadow bg-white absolute`}>
+                    {boxItems.map(item => (
+                        <div key={item.id} className="w-full h-1/2 py-1 flex items-center gap-x-2">
+                            <div className="w-7 h-7 rounded-full bg-yellow-300"></div>
+                            <h1>{i18n.language == "en" ? item.name[0] : item.name[1]}</h1>
+                        </div>
+                    ))}
+                </div>
+                <img className={`h-[350px] w-full ${i18n.language === "en" ? "reverse-img" : ""}`} src={mannequin_heroSection} alt="" />
             </div>
         </div>
     )
