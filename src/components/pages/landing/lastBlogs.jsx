81c350d1a4d29797e5743632158e8487e0e837c1
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:42:55 +0100

    Config with i18n and use Label Component

--- src/components/pages/landing/lastBlogs.jsx
+++ src/components/pages/landing/lastBlogs.jsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next"
+import { Label } from "../../common"
+
 const LastBlogs = () => {
+  const { t } = useTranslation()
   return (
     <div className="w-full py-28 lg:px-44 sm:px-16 px-8 flex flex-wrap gap-y-4 justify-center bg-gradientBackground bg-cover bg-center bg-no-repeat">
-      <span className="tag">اخبار و وبلاگ ها</span>
-      <h1 className="title-text text-center">آخرین خبر ما</h1>
-      <p className="description-text">هنگامی که چاپگر شناخته شده یک گالری از نوع scrambl edmake گرفت</p>
+      <Label text={t("lastBlogsLabel")} variant="section" />
+      <h1 className="boldStyle_text w-full text-center">{t("lastBlogsTitle")}</h1>
+      <p className="mediumStyle_text text-center">{t("lastBlogsDesc")}</p>
       <div className="w-full h-80 bg-yellow-500"></div>
     </div>
   )
