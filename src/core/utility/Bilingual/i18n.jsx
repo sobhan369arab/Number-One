4c13ed9d21f08edcde2b033b119c232663510eb3
Molaei <molaie.web.developer@gmail.com>
2024-08-26 00:47:06 +0100

    add new words

--- src/core/utility/Bilingual/i18n.jsx
+++ src/core/utility/Bilingual/i18n.jsx
@@ -13,8 +13,8 @@ i18n.use(initReactI18next)
                     // name Group
                     nameGroup: 'نامبر وان', teamSlogan: 'همیشه بهترین',
                     // Others
-                    yes: 'بله', no: 'خیر', Reviews: 'امتیاز', student: 'دانشجو', Development: 'توسعه', By: 'توسط',map:'نقشه',
-                    test:'این یک پیام تستی است',
+                    yes: 'بله', no: 'خیر', Reviews: 'امتیاز', student: 'دانشجو', Development: 'توسعه', By: 'توسط', map: 'نقشه',
+                    test: 'این یک پیام تستی است',
                     // Menu Items in the Header
                     HomeSection: 'صفحه اصلی', CoursesSection: 'دوره های آموزشی', Events: 'ایونت ها', ShopSection: 'فروشگاه', BlogSection: 'وبلاگ',
                     // Search Input in the Header
@@ -25,6 +25,30 @@ i18n.use(initReactI18next)
                     // Links
                     linksTitle: 'لینک های مفید', link1: 'ارزش های ما', link2: 'هیئت مشاوران ما', link3: 'شرکای ما',
                     link4: 'شریک شدن', link5: 'در Future Learn کار کنید', link6: 'Quizlet Plus',
+                    // Landing
+                        // heroSection
+                    heroSectionDescription: "هر سفر آموزشی و یادگیری دنبال کردن منحصر به فرد است ما به شما کمک خواهیم کرد", heroSectionTitle1: "هرگز از",
+                    heroSectionTitle2: "دست نکشید زندگی هرگز از آموزش دست نمی کشد", heroSectionBtn: "رایگان آزمایش کنید",
+                        // category
+                    categoryLabel: "دسته بندی های پرطرفدار", categoryTitle: "دسته بندی های برتر", categoryDescription: "امروزه به دلیل آن که ارتباطات فضای مجازی رونق زیادی یافته است",
+                        // aboutUs
+                    aboutLabel: "درباره ما بیشتر بدانید", aboutTitle1: "هزاران", aboutTitle2: "برتر اکنون در یک مکان", 
+                    aboutDescription: "صندوق ورودی مشترک بصری Groove این کار را برای اعضای تیم آسان می کند سازماندهی، اولویت بندی و. در این قسمت از Smashing Pod ما هستیم در مورد پایه پلتفرم وب.", 
+                    aboutBtn: "رایگان آزمایش کنید", aboutImportantWord: "دوره های", boardText: "بیش از 36 هزار دانشجوی ثبت نام شده",
+                        // topCourses
+                    topCoursesLabel: "دوره های کلاس برتر", topCoursesTitle: "بهترین دوره های آموزشی جهان ما را کاوش کنید", topCoursesDescription: "امروزه به دلیل آن که ارتباطات فضای مجازی رونق زیادی یافته است",
+                        // skilledTeachers
+                    skilledTeachersLabel: "معلم های ماهر", skilledTeachersTitle: "کلاس برتر ما و مربیان خبره در یک مکان", 
+                    skilledTeachersDesc: "هنگامی که یک چاپگر ناشناس یک گالری از نوع و کتاب نمونه درهم درست شده باقی نمانده است فقط پنج قرن", skilledTeachersBtn: "همه مربیان را ببینید",
+                        // faq
+                    faqLabel: "سوالات متداول", faqTitle: "شروع به تمرین از مربیان حرفه ای جهان", faqDescription: "صندوق ورودی مشترک بصری Groove این کار را برای اعضای تیم آسان می کند دهی، اولویت بندی و.در این قسمت.",
+                        // howToStart
+                    howToStartLabel: "چگونه سفر را شروع می کنیم", howToStartTitle: "سفر یادگیری خود را از همین امروز شروع کنید!", howToStartDesc: "اعضای شهودی Groove make team ورودی را با هم به تقسیم سازماندهی، اولویت بندی و.در این قسمت.",
+                        // lastBlogs
+                    lastBlogsLabel: "اخبار و وبلاگ ها", lastBlogsTitle: "آخرین خبر ما", lastBlogsDesc: "هنگامی که چاپگر شناخته شده یک گالری از نوع scrambl edmake گرفت",
+                    // informedPoster
+                    informedPlaceholder: "ایمیل خود را تایپ کنید", informedWord1: "میخواهید در مورد", informedWord3: "جدید",
+                    informedImportantWord1: "دوره های", informedImportantWord2: "مطلع باشید", informedBtn: "اکنون مشترک شوید",
                     // Company
                     companyTitle: 'شرکت ما', company1: 'با ما تماس بگیرید', company2: 'معلم شوید',
                     company3: 'وبلاگ', company4: 'مربی', company5: 'مناسبت ها',
@@ -68,8 +92,8 @@ i18n.use(initReactI18next)
                     // name Group
                     nameGroup: 'Number One', teamSlogan: 'ALWAYS THE BEST',
                     // Others
-                    yes: 'Yes', no: 'No', Reviews: 'Reviews', student: 'Students', Development: 'Development', By: 'By',map:'Map',
-                    test:'This is a test message',
+                    yes: 'Yes', no: 'No', Reviews: 'Reviews', student: 'Students', Development: 'Development', By: 'By', map: 'Map',
+                    test: 'This is a test message',
                     // Menu Items in the Header
                     HomeSection: 'Home', CoursesSection: 'Courses', Events: 'Events', ShopSection: 'Shop', BlogSection: 'Blog',
                     // Search Input in the Header
@@ -80,6 +104,30 @@ i18n.use(initReactI18next)
                     // Links
                     linksTitle: 'Useful Links', link1: 'Our values', link2: 'Our advisory board', link3: 'Our partners',
                     link4: 'Become a partner', link5: 'Work at Future Learn', link6: 'Quizlet Plus',
+                    // Landing
+                        // heroSection
+                    heroSectionDescription: "Every teaching and learning journey is unique Following We'll help guide your way.", heroSectionTitle1: "Never Stop",
+                    heroSectionTitle2: "Life Never Stop Teaching", heroSectionBtn: "Start Free Trial",
+                        // category
+                    categoryLabel: "Trending Categories", categoryTitle: "Top Category We Have", categoryDescription: "when known printer took a galley of type scrambl edmake",
+                        // aboutUs
+                    aboutLabel: "Get More About Us", aboutTitle1: "Thousand Of Top", aboutTitle2: "Now in One Place", 
+                    aboutDescription: "Groove’s intuitive shared inbox makes it easy for team members to organize, prioritize and.In this episode of the Smashing Pod we’re alking about Web Platform Baseline.", 
+                    aboutBtn: "Start Free Trial", aboutImportantWord: "Courses", boardText: "36K+ Enrolled Students",
+                        // topCourses
+                    topCoursesLabel: "Top Class Courses", topCoursesTitle: "Explore Our World's Best Courses", topCoursesDescription: "When known printer took a galley of type scrambl edmake",
+                        //skilledTeachers
+                    skilledTeachersLabel: "Skilled Introduce", skilledTeachersTitle: "Our Top Class & Expert Instructors in One Place", 
+                    skilledTeachersDesc: "when an unknown printer took a galley of type and scrambled makespecimen book has survived not only five centuries", skilledTeachersBtn: "See All Instructors",
+                        // faq
+                    faqLabel: "Faq’s", faqTitle: "Start Learning From World’s Pro Instructors", faqDescription: "Groove’s intuitive shared inbox makes it easy for team members to organize, prioritize and.In this episode.",
+                        // howToStart
+                    howToStartLabel: "How We Start Journey", howToStartTitle: "Start your Learning Journey Today!", howToStartDesc: "Groove’s intuitive shared inbox makesteam members together organize, prioritize and.In this episode.",
+                        // lastBlogs
+                    lastBlogsLabel: "News & Blogs", lastBlogsTitle: "Our Latest News Feed", lastBlogsDesc: "when known printer took a galley of type scrambl edmake",
+                    // informedPoster
+                    informedPlaceholder: "Type your e-mail", informedWord1: "Want To Stay About", informedWord3: "New",
+                    informedImportantWord1: "Informed", informedImportantWord2: "Courses & Study?", informedBtn: "Subscribe Now",
                     // Company
                     companyTitle: 'Our Company', company1: 'Contact Us', company2: 'Become Teacher',
                     company3: 'Blog', company4: 'Instructor', company5: 'Events',
