604fbdef39bb87c21262c4f1635a99451cf1e6aa
Molaei <molaie.web.developer@gmail.com>
2024-08-28 09:03:40 +0100

    Export components about userPanel

---
+++ src/components/pages/user-panel/index.js
@@ -0,0 +1,15 @@
+import SidebarUserPanel from "./SidebarUserPanel";
+import DarkModeBtn from "./DarkModeBtn";
+import UserCourseStatus from "./dashboard/UserCourseStatus";
+import LatestBlogs from "./dashboard/LatestBlogs";
+import UserCoursesSection from "./dashboard/UserCoursesSection";
+import CreateCourse from "./dashboard/CreateCourse";
+
+export {
+  SidebarUserPanel,
+  DarkModeBtn,
+  UserCourseStatus,
+  LatestBlogs,
+  UserCoursesSection,
+  CreateCourse
+};
