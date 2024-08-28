2759bc4e035caefda688df38f372ccbe1c412260
Molaei <molaie.web.developer@gmail.com>
2024-08-28 09:09:47 +0100

    export new pages

--- src/screen/index.js
+++ src/screen/index.js
@@ -2,5 +2,26 @@ import Error404 from "./error-page/Error404";
 import EventDetails from "./events-detail/EventDetails";
 import EventList from "./events/Events";
 import ContactUs from "./contact-us/ContactUs";
+import Dashboard from "./user-panel/Dashboard";
+import Favorites from "./user-panel/Favorites";
+import Information from "./user-panel/Information";
+import MyCourses from "./user-panel/MyCourses";
+import MyViews from "./user-panel/MyViews";
+import Reserved from "./user-panel/Reserved";
+import Security from "./user-panel/Security";
+import Landing from "./landing/landing";
 
-export { Error404, EventDetails, EventList, ContactUs };
+export {
+  Error404,
+  EventDetails,
+  EventList,
+  ContactUs,
+  Landing,
+  Dashboard,
+  Favorites,
+  Information,
+  MyCourses,
+  MyViews,
+  Reserved,
+  Security,
+};
