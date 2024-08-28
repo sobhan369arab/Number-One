395f3d388b5cd513aa0e99c78f9cabd53e0cb275
Molaei <molaie.web.developer@gmail.com>
2024-08-28 09:00:32 +0100

    install next ui package and config this package in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
-        'heroSection': "url('./src/assets/images/hero_section_landing.jpg')",
-        'importantWord': "url('./src/assets/images/importantWord.png')",
-        'informedPoster': "url('./src/assets/images/informedPoster.jpg')",
-        'gradientBackground': "url('./src/assets/images/gradientBackground.jpg')",
-        'titleSectionGradient': "url('./src/assets/images/titleSectionGradient.jpg')"
+        heroSection: "url('./src/assets/images/hero_section_landing.jpg')",
+        importantWord: "url('./src/assets/images/importantWord.png')",
+        informedPoster: "url('./src/assets/images/informedPoster.jpg')",
+        gradientBackground: "url('./src/assets/images/gradientBackground.jpg')",
+        titleSectionGradient:
+          "url('./src/assets/images/titleSectionGradient.jpg')",
       },
       colors: {
         footerColor: "#06042E",
@@ -19,12 +26,24 @@ export default {
       fontFamily: {
         IranSans: ["Iran-Sans"],
         Pop_Med: ["Pop-Medium"],
-        Sahel: ['"Pop-Medium"', '"Sahel"']
+        Sahel: ['"Pop-Medium"', '"Sahel"'],
       },
       screens: {
-        'mobile': {'max': '690px'}
+        mobile: { max: "690px" },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#5751E1",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
