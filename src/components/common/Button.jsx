c8cf3a0266ab6848fa7998b7bd56316314a181c2
Molaei <molaie.web.developer@gmail.com>
2024-08-29 09:20:41 +0100

    change code for set any icon to this button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -6,13 +6,13 @@ export const variantButton = {
     "yellow": "bg-yellowCustom text-black buttonYellow_shadow",
 }
 
-const Button = ({ text, arrowColor, variant, style }) => {
+const Button = ({ text, arrowColor, variant, style, Icon }) => {
     const { t, i18n } = useTranslation();
     return (
         <button className={`rounded-3xl pb-3 pt-1.5 px-5 flex items-center gap-x-2 duration-200 ${variantButton?.[variant]} ${style}`}>
             <span className="text-sm">{t(text)}</span>
             <div className={i18n.language === "en" ? "reverse-img" : ""}>
-                <ArrowLeftIcon stroke={arrowColor} height="12px" />
+                {Icon ? <Icon /> : <ArrowLeftIcon stroke={arrowColor} height="12px" />}
             </div>
         </button>
     )
