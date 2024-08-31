fea8ee63555532ba667f3e72e07e4fb1e9fdf2a8
sobhan369arab <sobhanarab30@gmail.com>
2024-08-31 11:39:10 +0430

    create Comparison Button

---
+++ src/components/common/ComparisonBtn.jsx
@@ -0,0 +1,11 @@
+import { ComparisonIcon } from "../../core/icon"
+
+const ComparisonBtn = () => {
+  return (
+    <div className="p-1 absolute bg-white rounded-xl right-5 top-5"> 
+        <ComparisonIcon  className={"h-8"}/>
+    </div>
+  )
+}
+
+export default ComparisonBtn
