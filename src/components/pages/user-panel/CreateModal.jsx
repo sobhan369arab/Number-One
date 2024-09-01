ccaeab7338f648b2aaa2632a72daf0fdc9f67f6b
Molaei <molaie.web.developer@gmail.com>
2024-09-01 06:35:46 +0100

    create modal component

---
+++ src/components/pages/user-panel/CreateModal.jsx
@@ -0,0 +1,15 @@
+import { Modal, ModalContent, ModalBody } from "@nextui-org/react"
+
+const CreateModal = ({ children, isOpen, onClose }) => {
+    return (
+        <Modal size="4xl" backdrop={"blur"} isOpen={isOpen} onClose={onClose} hideCloseButton>
+            <ModalContent className="bg-[#525252]/70 p-3">
+                <ModalBody className="flex flex-row flex-wrap justify-center gap-y-10 py-20 px-32 relative">
+                    {children}
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default CreateModal
