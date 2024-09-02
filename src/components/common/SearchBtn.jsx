d91588c019d09f66ec71403abbf1824e659ef9e6
Molaei <molaie.web.developer@gmail.com>
2024-09-02 22:16:15 +0100

    share this button and change place

---
+++ src/components/common/SearchBtn.jsx
@@ -0,0 +1,11 @@
+import { SearchIcon } from "../../core/icon"
+
+const SearchBtn = () => {
+    return (
+        <div className="w-9 h-9 bg-purpleCustom rounded-full flex justify-center items-center rotate-[88deg]">
+            <SearchIcon width="25px" />
+        </div>
+    )
+}
+
+export default SearchBtn
