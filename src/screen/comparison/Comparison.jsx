4fdd31be5cf733d9b2d93cb7472cbf8df71e60a4
sobhan369arab <sobhanarab30@gmail.com>
2024-09-07 22:27:05 +0430

    Filtering and getting objects for comparison

--- src/screen/comparison/Comparison.jsx
+++ src/screen/comparison/Comparison.jsx
@@ -1,23 +1,37 @@
 import { useLocation, useSearchParams } from "react-router-dom"
-import { HeadingSection } from "../../components/pages/comparison"
+import { HeadingSection, ItemsCompared } from "../../components/pages/comparison"
 import TitleSection from "../../components/partials/title-section/TitleSection"
 import { useEffect } from "react"
+import { CoursesDataFa } from "../../core/constants/Courses/courses-data_Fa"
+import { useState } from "react"
 
 const Comparison = () => {
   const [searchParams, setSearchParams] = useSearchParams({ C: [] })
   const location = useLocation();
+  const [object, setObject] = useState();
 
   useEffect(() => {
     if (location.state === null) {
-      console.log(location.state)
       setSearchParams({ C: location.state })
     }
+    getObj();
   }, [location.state])
+
+  const getObj = () => {
+    var CoursesFiltered = [];
+    for (let Id of location.state) {
+      CoursesFiltered.push(
+        CoursesDataFa.filter(el => el.id === Id)
+      )
+    }
+    setObject(CoursesFiltered.flat(Infinity))
+  }
 
   return (
     <>
       <TitleSection title={'comparisonTitle'} />
       <HeadingSection />
+      <ItemsCompared/>
     </>
   )
 }
