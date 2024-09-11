431158a451ff72898f6b5249903a94e9492d7fd6
sobhan369arab <sobhanarab30@gmail.com>
2024-09-11 22:12:46 +0430

    Correcting file names

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,6 +1,6 @@
 import Error404 from "./error-page/Error404";
 import EventDetails from "./events-detail/EventDetails";
-import EventList from "./Events/Events";
+import EventList from "./events/Events";
 import ContactUs from "./contact-us/ContactUs";
 import Instructors from "./instructors/Instructors";
 import InstructorDetail from "./instructors-detail/InstructorDetail";
