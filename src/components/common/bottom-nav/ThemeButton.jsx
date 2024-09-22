1189b065c1758f9900613df6f14e59076bd679bc
Molaei <molaie.web.developer@gmail.com>
2024-09-23 00:11:40 +0100

    button change theme

---
+++ src/components/common/bottom-nav/ThemeButton.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { BrushIcon, MoonIcon, SunIcon } from "../../../core/icon"
+import { useSelector, useDispatch } from "react-redux"
+import { changeThemeAction } from "../../../redux/slices/DarkMode"
+
+const ThemeButton = () => {
+    const theme = useSelector(state => state.DarkMode)
+    const iconProps = { width: "24px", height: "24px", fill: "#fff" }
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (theme === true) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme])
+
+    return (
+        <div onClick={() => { dispatch(changeThemeAction()) }} className="bottomNav">
+            {theme ? <MoonIcon {...iconProps} /> : <SunIcon {...iconProps} />}
+        </div>
+    )
+}
+
+export default ThemeButton
