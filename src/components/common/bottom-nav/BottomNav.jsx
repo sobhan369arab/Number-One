0a55bb93fb3168535666c0dbd056419fdf39056b
Molaei <molaie.web.developer@gmail.com>
2024-09-23 04:11:26 +0100

    add scroll to top

--- src/components/common/bottom-nav/BottomNav.jsx
+++ src/components/common/bottom-nav/BottomNav.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LanguageButton from './LanguageButton'
 import ThemeButton from './ThemeButton'
+import ScrollToUpButton from './ScrollToUpButton'
 
 const BottomNav = () => {
     return (
         <div className="flex flex-col flex-wrap gap-3 fixed left-4 bottom-20 z-50">
             <ThemeButton />
             <LanguageButton />
+            <ScrollToUpButton />
             <span className='w-10 h-3 bottomNav_shadow'></span>
         </div>
     )
