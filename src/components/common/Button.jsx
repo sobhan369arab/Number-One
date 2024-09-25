6878710cc0cf35a3bae2f8d3fdb0aa1efa846cf1
Molaei <molaie.web.developer@gmail.com>
2024-09-26 00:29:10 +0100

    fix bug

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -11,14 +11,14 @@ const Button = ({ text, arrowColor, vStyle, style, Icon, href, vType, isClick,di
     const variantType = {
         link: (
             <Link to={href} className={`rounded-3xl pb-3 pt-1.5 px-5 flex items-center gap-x-2 duration-200 ${variantStyle?.[vStyle]} ${style}`}>
-                <span className="text-sm font-semibold">{t(text)}</span>
+                <span className="text-sm">{t(text)}</span>
                 <div className={`${i18n.language === "en" ? "reverse-img" : ""} ${disableArrow}`}>
                     {Icon ? <Icon /> : <ArrowLeftIcon stroke={arrowColor} height="12px" />}
                 </div>
             </Link>
         ),
         button: (
-            <button onClick={isClick} type='submit' className={`pb-3 pt-1.5 border-2  border-black px-5 flex items-center gap-x-2 rounded-full duration-200 ${variantStyle?.[vStyle]} ${style}`}>
+            <button onClick={isClick} type='submit' className={`pb-3 pt-1.5 border-2 border-black px-5 flex items-center gap-x-2 rounded-full duration-200 ${variantStyle?.[vStyle]} ${style}`}>
                 {t(text)}
                 <div className={i18n.language === "en" ? "reverse-img" : ""}>
                     {Icon ? <Icon /> : <ArrowLeftIcon stroke={arrowColor} height="12px" />}
