17cd7674f06c8ef6de10a0309608325a535b8952
Molaei <molaie.web.developer@gmail.com>
2024-09-26 00:02:46 +0100

    fix bugs

--- src/components/common/bottom-nav/BottomNav.jsx
+++ src/components/common/bottom-nav/BottomNav.jsx
@@ -13,7 +13,6 @@ const BottomNav = () => {
             </div>
             <div className='flex flex-col gap-3 fixed right-5 bottom-20 z-50'>
                 <ScrollToUpButton />
-                <span className='w-10 h-3 bottomNav_shadow'></span>
             </div>
         </>
     )
