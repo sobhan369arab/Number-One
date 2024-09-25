76107b8b6fb7f33e0fc1b23a77b5c7f81250018a
Molaei <molaie.web.developer@gmail.com>
2024-09-25 08:57:37 +0100

    add shadow

--- src/components/common/bottom-nav/BottomNav.jsx
+++ src/components/common/bottom-nav/BottomNav.jsx
@@ -6,13 +6,14 @@ import ScrollToUpButton from './ScrollToUpButton'
 const BottomNav = () => {
     return (
         <>
-            <div className="flex flex-col flex-wrap gap-3 fixed left-5 bottom-20 z-50">
+            <div className="flex flex-col gap-3 fixed left-5 bottom-20 z-50">
                 <ThemeButton />
                 <LanguageButton />
                 <span className='w-10 h-3 bottomNav_shadow'></span>
             </div>
-            <div className='fixed right-6 bottom-24 z-50'>
+            <div className='flex flex-col gap-3 fixed right-5 bottom-20 z-50'>
                 <ScrollToUpButton />
+                <span className='w-10 h-3 bottomNav_shadow'></span>
             </div>
         </>
     )
