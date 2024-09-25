8853ddc845e3596f2c1615cfe78eb4c864249c04
sobhan369arab <sobhanarab30@gmail.com>
2024-09-25 20:02:37 +0330

    scroll to up

--- src/components/common/bottom-nav/BottomNav.jsx
+++ src/components/common/bottom-nav/BottomNav.jsx
@@ -5,12 +5,16 @@ import ScrollToUpButton from './ScrollToUpButton'
 
 const BottomNav = () => {
     return (
-        <div className="flex flex-col flex-wrap gap-3 fixed left-4 bottom-20 z-50">
-            <ThemeButton />
-            <LanguageButton />
-            <ScrollToUpButton />
-            <span className='w-10 h-3 bottomNav_shadow'></span>
-        </div>
+        <>
+            <div className="flex flex-col flex-wrap gap-3 fixed left-5 bottom-20 z-50">
+                <ThemeButton />
+                <LanguageButton />
+                <span className='w-10 h-3 bottomNav_shadow'></span>
+            </div>
+            <div className='fixed right-6 bottom-24 z-50'>
+                <ScrollToUpButton />
+            </div>
+        </>
     )
 }
 
