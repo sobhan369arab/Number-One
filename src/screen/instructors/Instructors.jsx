ac854b909f2c9d168f555816a9d39313b0af35da
Molaei <molaie.web.developer@gmail.com>
2024-09-27 04:43:44 +0100

    fix bugs

--- src/screen/instructors/Instructors.jsx
+++ src/screen/instructors/Instructors.jsx
@@ -1,19 +1,35 @@
 import TitleSection from "../../components/partials/title-section/TitleSection"
-import { TeacherCard } from "../../components/common"
+import { calculatePageCount, handlePageClick, PaginatedItems, PaginateHolderItems, TeacherCard } from "../../components/common"
 import { teachers } from "../../core/constants/instructors/teachers"
 import { useTranslation } from "react-i18next"
 import BreadCrumb from "../../components/partials/title-section/BreadCrumb"
+import { useState } from "react"
 
 const Instructors = () => {
     const { t } = useTranslation()
+    const [itemOffset, setItemOffset] = useState(0);
+
+    const endOffset = itemOffset + 9;
+    const currentItems = teachers.slice(itemOffset, endOffset);
     return (
         <>
             <TitleSection title={t("instructorsTitle")} >
-                <BreadCrumb type="Div" text="instructorsTitle"/>
+                <BreadCrumb type="Div" text="instructorsTitle" />
             </TitleSection>
-            <div className="lg:px-44 sm:px-16 px-8 w-full flex justify-evenly gap-x-12 gap-y-8 flex-wrap my-20">
-                {teachers.map(item => <TeacherCard key={item.id} name={item.name} skill={item.skill} href={`/instructorsDetails/${item.id}`} />)}
-            </div>
+            <PaginateHolderItems style="justify-center">
+                <PaginatedItems handlePageClick={(event) => { handlePageClick(event, 9, setItemOffset, teachers) }} pageCount={calculatePageCount(teachers, 9)}>
+                    <div className="lg:px-44 sm:px-16 px-8 w-full flex justify-evenly gap-x-12 gap-y-8 flex-wrap my-20">
+                        {currentItems.map(item => (
+                            <TeacherCard
+                                key={item.id}
+                                name={item.name}
+                                skill={item.skill}
+                                href={`/instructorsDetails/${item.id}`}
+                            />
+                        ))}
+                    </div>
+                </PaginatedItems>
+            </PaginateHolderItems>
         </>
     )
 }
