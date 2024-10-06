01016c2503ba1e7200bcb60dff4efaaad1c89088
Molaei <molaie.web.developer@gmail.com>
2024-10-06 22:42:15 +0100

    develop so comment

--- src/components/common/comment/comment-item/TopSection.jsx
+++ src/components/common/comment/comment-item/TopSection.jsx
@@ -1,10 +1,10 @@
 
-const TopSection = ({ title, describe, Style }) => {
+const TopSection = ({ name, date, title, describe, Style }) => {
   return (
     <div>
       <div className='flex justify-between'>
-        <h1 className='text-xl font-semibold'>مریم</h1>
-        <p className='mediumStyle_text text-sm'>21 تیر 1403</p>
+        <h1 className='text-xl font-semibold'>{name}</h1>
+        <p className='mediumStyle_text text-sm'>{date && date.slice(0, 10)}</p>
       </div>
       <h1 className={`text-lg font-semibold mt-3 ${Style}`}>{title}</h1>
       <h2 className='mediumStyle_text'>{describe}</h2>
