45de56b8c9e3e0925db9d37ffb8c867dc41a6ccd
Molaei <molaie.web.developer@gmail.com>
2024-10-06 22:30:25 +0100

    add react query

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,20 +7,33 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 import { BottomNav, ToastAlert } from "../components/common";
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
   // const [isLogin, setLogin] = useState(false);
   // const router = createBrowserRouter(isLogin ? routerPublic : routerPublic);
   const { i18n } = useTranslation()
   const notify = () => toast.success("Wow so easy !");
+
+  // Create a client
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  })
+
   return (
     <Provider store={store}>
       <NextUIProvider>
-        <main dir={i18n.language === 'fa' ? 'rtl' : 'ltr'} className={`bg-MainBg  ${i18n.language === 'fa' ? 'font-IranSans' : 'font-Pop_Med'}`}>
-          <ToastAlert />
-          <RouterProvider router={routerPublic} />
-          <BottomNav />
-        </main>
+        <QueryClientProvider client={queryClient}>
+          <main dir={i18n.language === 'fa' ? 'rtl' : 'ltr'} className={`bg-MainBg  ${i18n.language === 'fa' ? 'font-IranSans' : 'font-Pop_Med'}`}>
+            <ToastAlert />
+            <RouterProvider router={routerPublic} />
+            <BottomNav />
+          </main>
+        </QueryClientProvider>
       </NextUIProvider>
     </Provider>
   )
