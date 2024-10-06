1a871c9c9aac078f922cd983f5edd42516a4beb9
Molaei <molaie.web.developer@gmail.com>
2024-10-06 22:39:45 +0100

    add a bet for like or dislike

--- src/components/common/ToLike.jsx
+++ src/components/common/ToLike.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import { BiDislike, BiLike, BiSolidDislike, BiSolidLike } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const ToLike = ({
     likeNumber,
     disLikeNumber,
     numberStatus,
 }) => {
-
+    const user = useSelector(state => state.UserInfo.info)
     const [like, setLike] = useState(false);
     const [disLike, setDisLike] = useState(false);
     const [LikeCount, setLikeCount] = useState(likeNumber);
@@ -14,7 +16,9 @@ const ToLike = ({
 
 
     const handleLike = () => {
+        if (!user) return toast.error("لطفا اول وارد حساب کاربری خود شوید!")
         setLike(!like);
+        
         if (like === false) {
             setLikeCount(LikeCount + 1)
             if (disLike === true) {
@@ -27,6 +31,8 @@ const ToLike = ({
         }
     }
     const handleDisLike = () => {
+        if (!user) return toast.error("لطفا اول وارد حساب کاربری خود شوید!")
+
         setDisLike(!disLike);
         if (disLike === false) {
             setDisLikeCount(dislikeCount + 1)
@@ -43,11 +49,11 @@ const ToLike = ({
         <div className="flex gap-1.5">
             <div onClick={() => handleLike()} className="px-2 py-0.5 bg-LightLavender mediumStyle_text flex gap-0.5 items-center rounded-2xl cursor-pointer">
                 {like == false ? <BiLike /> : <BiSolidLike />}
-                <span className={numberStatus}>{LikeCount}</span>
+                <span className={numberStatus}>{likeNumber}</span>
             </div>
             <div onClick={() => handleDisLike()} className="px-2 py-0.5 bg-LightLavender mediumStyle_text flex gap-0.5 items-center rounded-2xl cursor-pointer">
                 {disLike == false ? <BiDislike /> : <BiSolidDislike />}
-                <span className={numberStatus}>{dislikeCount}</span>
+                <span className={numberStatus}>{disLikeNumber}</span>
             </div>
         </div>
     )
