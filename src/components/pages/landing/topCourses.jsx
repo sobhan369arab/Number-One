a72070ea38bcdb24bae4382c8ca18d4d863f10ed
sobhan369arab <sobhanarab30@gmail.com>
2024-10-09 09:36:23 +0330

    react-query for courses in the landing

--- src/components/pages/landing/topCourses.jsx
+++ src/components/pages/landing/topCourses.jsx
@@ -6,13 +6,15 @@ import { useEffect, useState } from "react"
 import Course from "../course/Course"
 import { SwiperSlide } from "swiper/react"
 import { GetAllCourseByPagination, GetTechnologies } from "../../../core/services/api/GetData"
+import { useQuery } from "@tanstack/react-query"
 
 const TopCourses = () => {
     const { t, i18n } = useTranslation()
-    const [tabs, setTabs] = useState([])
-    const [courseList, setCourseList] = useState([])
+    // const [tabs, setTabs] = useState([])
+    // const [courseList, setCourseList] = useState([])
     const theme = useSelector(state => state.DarkMode)
     const [tabValue, setTabValue] = useState(2)
+    const skeletonData = [{}, {}, {}, {}];
 
     const Breakpoints = {
         1024: { spaceBetween: 20, slidesPerView: 4 },
@@ -20,24 +22,57 @@ const TopCourses = () => {
         300: { spaceBetween: 20, slidesPerView: 1 },
     }
 
-    const getTabs = async () => {
-        setTabs(await GetTechnologies())
+    // const getTabs = async () => {
+    //     setTabs(await GetTechnologies())
+    // }
+
+    // useEffect(() => { getTabs() }, [])
+    const { data: tabData } = useQuery({
+        queryKey: 'GET_TABS',
+        queryFn: async () => {
+            return await GetTechnologies();
+        }
+    })
+
+    const apiParams = { TechCount: 1, ListTech: tabValue }
+    const { data: courseData, isLoading, isSuccess } = useQuery({
+        queryKey: 'GET_COURSES',
+        queryFn: async () => {
+            return await GetAllCourseByPagination(apiParams)
+        }
+    })
+
+    const CourseReturn = () => {
+        if (isLoading) {
+            return (
+
+                skeletonData.map((item, index) => {
+                    <SwiperSlide key={index}>
+                        <Course key={index} item={item} isLoading={isLoading} />
+                    </SwiperSlide>
+                })
+            )
+        }
+        else if (isSuccess) {
+            return courseData?.courseFilterDtos.map((item, index) => {
+                <SwiperSlide key={index}>
+                    <Course key={index} item={item} isLoading={isLoading} />
+                </SwiperSlide>
+            })
+        }
     }
 
-    useEffect(() => { getTabs() }, [])
+    // const getCourseList = async () => {
 
-    const getCourseList = async () => {
-        const apiParams = { TechCount: 1, ListTech: tabValue }
+    //     const courseData = await GetAllCourseByPagination(apiParams)
+    //     setCourseList(courseData.courseFilterDtos)
+    // }
 
-        const courseData = await GetAllCourseByPagination(apiParams)
-        setCourseList(courseData.courseFilterDtos)
-    }
+    // useEffect(() => {
+    //     getCourseList()
+    // }, [tabValue])
 
-    useEffect(() => {
-        getCourseList()
-    }, [tabValue])
-
-    const [comparisonId, setComparisonId] = useState([])
+    // const [comparisonId, setComparisonId] = useState([])
 
     return (
         <div className={`w-full py-28 lg:px-44 sm:px-16 px-8 flex flex-wrap gap-y-4 justify-center ${theme ? "bg-gradientBackgroundDark" : "bg-gradientBackground"} bg-cover bg-center bg-no-repeat`}>
@@ -45,10 +80,11 @@ const TopCourses = () => {
             <h1 className="boldStyle_text w-full text-center">{t("topCoursesTitle")}</h1>
             <p className="mediumStyle_text w-full text-center">{t("topCoursesDescription")}</p>
             <div className="mobile:w-full sm:w-auto flex mobile:justify-evenly justify-center relative mobile:px-0 sm:px-20 mt-10">
-                {tabs.map(obj => <Tab key={obj.id} setTabValue={setTabValue} value={obj.id} name="landing" title={obj.techName} />)}
+                {tabData && tabData.map(obj => <Tab key={obj.id} setTabValue={setTabValue} value={obj.id} name="landing" title={obj.techName} />)}
                 <span className="w-full h-1 bg-LightGrayish rounded-full bottom-0 absolute"></span>
             </div>
             <div className="max-[680px]:w-[300px] w-[110%] mt-5 relative">
+                {CourseReturn()}
                 <SwiperSlider
                     perView={4}
                     Breakpoints={Breakpoints}
@@ -57,27 +93,6 @@ const TopCourses = () => {
                     buttonSideRight="top-[190px] xl:-right-[60px] sm:right-[20px] hidden sm:flex h-12 z-30 w-12"
                     buttonColor="bg-VioletBlue"
                 >
-                    {courseList != undefined && courseList.map((item, index) => (
-                        <SwiperSlide key={index}>
-                            <Course
-                                id={index}
-                                title={item.title}
-                                images={item.tumbImageAddress}
-                                instructor={item.teacherName}
-                                score={item.courseRate}
-                                category={item.technologyList}
-                                level={item.levelName}
-                                price={item.cost}
-                                date={item.lastUpdate}
-                                studentsNumber={0}
-                                like={item.likeCount}
-                                disLike={item.dissLikeCount}
-                                bio={item.describe}
-                                comparisonId={comparisonId}
-                                setComparisonId={setComparisonId}
-                            />
-                        </SwiperSlide>
-                    ))}
                 </SwiperSlider>
             </div>
         </div>
