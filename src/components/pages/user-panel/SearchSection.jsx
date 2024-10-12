0fcd7b2e850db39e07a87bebf0c509d43e5262d5
Molaei <molaie.web.developer@gmail.com>
2024-10-12 06:33:54 +0100

    change to dynamic

--- src/components/pages/user-panel/SearchSection.jsx
+++ src/components/pages/user-panel/SearchSection.jsx
@@ -1,19 +1,19 @@
 import { SearchInput, SortBox } from "../../common"
-import { setQuery, setSortingCol } from "../../../redux/slices/userPanel-filter-slices/MyCourses"
-import { sortOptionPanelMyCourses } from "../../../core/constants/sorts/Sort"
 import { useTranslation } from "react-i18next"
 
-const SearchSection = () => {
+const SearchSection = ({ sortItem, defaultKey, setState, query }) => {
     const { i18n } = useTranslation()
 
     return (
         <div className='w-full flex justify-between'>
-            <SearchInput setQuery={setQuery} maxResponsiveValue={709} minResponsiveValue={710} showSearchFilter={false} inputStyle="py-2.5 w-72 lg:w-[400px]" />
+            <SearchInput setQuery={query} maxResponsiveValue={709} minResponsiveValue={710} showSearchFilter={false} inputStyle="py-2.5 w-72 lg:w-[400px]" />
             <SortBox
                 radius="full"
-                setState={setSortingCol}
-                options={sortOptionPanelMyCourses}
-                placeholder="انتخاب کنید"
+                aria-label="select"
+                setState={setState}
+                options={sortItem}
+                placeholder={["انتخاب کنید", "Choose"]}
+                defaultKey={defaultKey}
                 classNames={{
                     base: "!w-auto",
                     trigger: "bg-transparent border border-LightGrayish hover:!bg-transparent duration-500 h-[47px] w-[180px]",
